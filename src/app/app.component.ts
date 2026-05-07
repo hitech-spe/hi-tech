f1d0eb055793ca9438e87a3e88985d3abc2a61be
Leonardo Mansueto <leonardo.mansueto@infocamere.it>
2026-05-07 20:12:19 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { TranslateService } from '@ngx-translate/core';
-import { RouterOutlet, Router, NavigationEnd } from "@angular/router";
-import { HeaderComponent } from "./shared/header/header.component";
-import { SpinnerComponent } from "./shared/spinner/spinner.component";
-import { FooterComponent } from "./shared/footer/footer.component";
-import { filter } from 'rxjs/operators';
+import {Component, Inject, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from '@angular/common';
+import {TranslateService} from '@ngx-translate/core';
+import {RouterOutlet, Router, NavigationEnd} from "@angular/router";
+import {HeaderComponent} from "./shared/header/header.component";
+import {SpinnerComponent} from "./shared/spinner/spinner.component";
+import {FooterComponent} from "./shared/footer/footer.component";
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +22,9 @@ import { filter } from 'rxjs/operators';
 export class AppComponent {
   title = 'hi-tech';
   private isBrowser: boolean;
+
+  showSplash = true;  // Controlla se il blocco esiste nell'HTML
+  fadeSplash = false; // Controlla l'animazione di dissolvenza
 
   constructor(
     private translate: TranslateService,
@@ -43,11 +46,25 @@ export class AppComponent {
           setTimeout(() => {
             const element = document.querySelector('#' + tree.fragment);
             if (element) {
-              element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+              element.scrollIntoView({behavior: 'smooth', block: 'start'});
             }
           }, 100);
         }
       }
     });
   }
+
+  ngOnInit() {
+    // Dopo 2 secondi (2000ms), facciamo sfumare la splash screen
+    setTimeout(() => {
+      this.fadeSplash = true;
+
+      // Dopo un altro mezzo secondo (il tempo della transizione CSS), la rimuoviamo del tutto
+      setTimeout(() => {
+        this.showSplash = false;
+      }, 500);
+
+    }, 2000);
+  }
+
 }
